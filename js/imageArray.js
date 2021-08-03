cf56e577d7ca425a2c579480658ba1576dc35344
Weston Taylor <westontaylor1999@gmail.com>
2021-08-02 21:20:44 -0700

    change: image array is now in it's own js file for ease of coding, add: development branch, preliminary div for the later requirements, second mode of button and ask input for trial and error.

---
+++ js/imageArray.js
@@ -0,0 +1,175 @@
+let imageArray = new Array();
+
+imageArray[0] = new Image();
+imageArray[0].src = 'images/magic8ball_1.png';
+imageArray[0].style.height  = '100px';
+imageArray[0].style.width = '100px';
+
+imageArray[1] = new Image();
+imageArray[1].src = 'images/magic8ball_2.png';
+imageArray[1].style.height  = '100px';
+imageArray[1].style.width = '100px';
+
+imageArray[2] = new Image();
+imageArray[2].src = 'images/magic8ball_3.png';
+imageArray[2].style.height  = '100px';
+imageArray[2].style.width = '100px';
+
+imageArray[3] = new Image();
+imageArray[3].src = 'images/magic8ball_4.png';
+imageArray[3].style.height  = '100px';
+imageArray[3].style.width = '100px';
+
+imageArray[4] = new Image();
+imageArray[4].src = 'images/magic8ball_5.png';
+imageArray[4].style.height  = '100px';
+imageArray[4].style.width = '100px';
+
+imageArray[5] = new Image();
+imageArray[5].src = 'images/magic8ball_6.png';
+imageArray[5].style.height  = '100px';
+imageArray[5].style.width = '100px';
+
+imageArray[6] = new Image();
+imageArray[6].src = 'images/magic8ball_7.png';
+imageArray[6].style.height  = '100px';
+imageArray[6].style.width = '100px';
+
+imageArray[7] = new Image();
+imageArray[7].src = 'images/magic8ball_8.png';
+imageArray[7].style.height  = '100px';
+imageArray[7].style.width = '100px';
+
+imageArray[8] = new Image();
+imageArray[8].src = 'images/magic8ball_9.png';
+imageArray[8].style.height  = '100px';
+imageArray[8].style.width = '100px';
+
+imageArray[9] = new Image();
+imageArray[9].src = 'images/magic8ball_10.png';
+imageArray[9].style.height  = '100px';
+imageArray[9].style.width = '100px';
+
+imageArray[10] = new Image();
+imageArray[10].src = 'images/magic8ball_11.png';
+imageArray[10].style.height  = '100px';
+imageArray[10].style.width = '100px';
+
+imageArray[11] = new Image();
+imageArray[11].src = 'images/magic8ball_12.png';
+imageArray[11].style.height  = '100px';
+imageArray[11].style.width = '100px';
+
+imageArray[12] = new Image();
+imageArray[12].src = 'images/magic8ball_13.png';
+imageArray[12].style.height  = '100px';
+imageArray[12].style.width = '100px';
+
+imageArray[13] = new Image();
+imageArray[13].src = 'images/magic8ball_14.png';
+imageArray[13].style.height  = '100px';
+imageArray[13].style.width = '100px';
+
+imageArray[14] = new Image();
+imageArray[14].src = 'images/magic8ball_15.png';
+imageArray[14].style.height  = '100px';
+imageArray[14].style.width = '100px';
+
+imageArray[15] = new Image();
+imageArray[15].src = 'images/magic8ball_16.png';
+imageArray[15].style.height  = '100px';
+imageArray[15].style.width = '100px';
+
+imageArray[16] = new Image();
+imageArray[16].src = 'images/magic8ball_17.png';
+imageArray[16].style.height  = '100px';
+imageArray[16].style.width = '100px';
+
+imageArray[17] = new Image();
+imageArray[17].src = 'images/magic8ball_18.png';
+imageArray[17].style.height  = '100px';
+imageArray[17].style.width = '100px';
+
+imageArray[18] = new Image();
+imageArray[18].src = 'images/magic8ball_19.png';
+imageArray[18].style.height  = '100px';
+imageArray[18].style.width = '100px';
+
+imageArray[19] = new Image();
+imageArray[19].src = 'images/magic8ball_20.png';
+imageArray[19].style.height  = '100px';
+imageArray[19].style.width = '100px';
+
+function addInput(type, placeholder) {
+    const input = document.createElement(type);
+    form.appendChild(input);
+    input.placeholder = placeholder;
+};
+
+askButton.addEventListener(('click'), function (event) {
+    event.preventDefault();
+    console.log(randomNumber);
+    console.log(askInputResponse);
+    switch(randomNumber) {
+        case 1:
+             document.body.appendChild(imageArray[0])
+            break;
+        case 2:
+             document.body.appendChild(imageArray[1])
+            break;
+        case 3:
+             document.body.appendChild(imageArray[2])
+            break;
+        case 4:
+             document.body.appendChild(imageArray[3])
+            break;
+        case 5:
+             document.body.appendChild(imageArray[4])
+            break;
+        case 6:
+             document.body.appendChild(imageArray[5])
+            break;
+        case 7:
+             document.body.appendChild(imageArray[6])
+            break;
+        case 8:
+             document.body.appendChild(imageArray[7])
+            break;
+        case 9:
+             document.body.appendChild(imageArray[8])
+            break;
+        case 10:
+             document.body.appendChild(imageArray[9])
+            break;
+        case 11:
+             document.body.appendChild(imageArray[10])
+            break;
+        case 12:
+             document.body.appendChild(imageArray[11])
+            break;
+        case 13:
+             document.body.appendChild(imageArray[12])
+            break;
+        case 14:
+             document.body.appendChild(imageArray[13])
+            break;
+        case 15:
+             document.body.appendChild(imageArray[14])
+            break;
+        case 16:
+             document.body.appendChild(imageArray[15])
+            break;
+        case 17:
+             document.body.appendChild(imageArray[16])
+            break;
+        case 18:
+             document.body.appendChild(imageArray[17])
+            break;
+        case 19:
+             document.body.appendChild(imageArray[18])
+            break;
+        case 20:
+             document.body.appendChild(imageArray[19])
+            break;
+    }
+});
--- js/overview.js
+++ js/overview.js
@@ -7,112 +7,17 @@ askButton.style.height = '25px';
 askButton.style.width = '100px';
 askButton.innerText = 'Ask Me';
 
+const askButton2 = document.createElement('button');
+askButton2.style.height = '25px';
+askButton2.style.width = '100px';
+// askButton2.innerText = 'I have the answers. The question is do you want to hear them?';
+askButton2.innerText = 'this is actually hard'
+
 const askInput = addInput('input', 'Ask me anything!');
 
 
+
 let randomNumber = Math.floor((Math.random() * 20) + 1);
-
-let imageArray = new Array();
-
-imageArray[0] = new Image();
-imageArray[0].src = 'images/magic8ball_1.png';
-imageArray[0].style.height  = '100px';
-imageArray[0].style.width = '100px';
-
-imageArray[1] = new Image();
-imageArray[1].src = 'images/magic8ball_2.png';
-imageArray[1].style.height  = '100px';
-imageArray[1].style.width = '100px';
-
-imageArray[2] = new Image();
-imageArray[2].src = 'images/magic8ball_3.png';
-imageArray[2].style.height  = '100px';
-imageArray[2].style.width = '100px';
-
-imageArray[3] = new Image();
-imageArray[3].src = 'images/magic8ball_4.png';
-imageArray[3].style.height  = '100px';
-imageArray[3].style.width = '100px';
-
-imageArray[4] = new Image();
-imageArray[4].src = 'images/magic8ball_5.png';
-imageArray[4].style.height  = '100px';
-imageArray[4].style.width = '100px';
-
-imageArray[5] = new Image();
-imageArray[5].src = 'images/magic8ball_6.png';
-imageArray[5].style.height  = '100px';
-imageArray[5].style.width = '100px';
-
-imageArray[6] = new Image();
-imageArray[6].src = 'images/magic8ball_7.png';
-imageArray[6].style.height  = '100px';
-imageArray[6].style.width = '100px';
-
-imageArray[7] = new Image();
-imageArray[7].src = 'images/magic8ball_8.png';
-imageArray[7].style.height  = '100px';
-imageArray[7].style.width = '100px';
-
-imageArray[8] = new Image();
-imageArray[8].src = 'images/magic8ball_9.png';
-imageArray[8].style.height  = '100px';
-imageArray[8].style.width = '100px';
-
-imageArray[9] = new Image();
-imageArray[9].src = 'images/magic8ball_10.png';
-imageArray[9].style.height  = '100px';
-imageArray[9].style.width = '100px';
-
-imageArray[10] = new Image();
-imageArray[10].src = 'images/magic8ball_11.png';
-imageArray[10].style.height  = '100px';
-imageArray[10].style.width = '100px';
-
-imageArray[11] = new Image();
-imageArray[11].src = 'images/magic8ball_12.png';
-imageArray[11].style.height  = '100px';
-imageArray[11].style.width = '100px';
-
-imageArray[12] = new Image();
-imageArray[12].src = 'images/magic8ball_13.png';
-imageArray[12].style.height  = '100px';
-imageArray[12].style.width = '100px';
-
-imageArray[13] = new Image();
-imageArray[13].src = 'images/magic8ball_14.png';
-imageArray[13].style.height  = '100px';
-imageArray[13].style.width = '100px';
-
-imageArray[14] = new Image();
-imageArray[14].src = 'images/magic8ball_15.png';
-imageArray[14].style.height  = '100px';
-imageArray[14].style.width = '100px';
-
-imageArray[15] = new Image();
-imageArray[15].src = 'images/magic8ball_16.png';
-imageArray[15].style.height  = '100px';
-imageArray[15].style.width = '100px';
-
-imageArray[16] = new Image();
-imageArray[16].src = 'images/magic8ball_17.png';
-imageArray[16].style.height  = '100px';
-imageArray[16].style.width = '100px';
-
-imageArray[17] = new Image();
-imageArray[17].src = 'images/magic8ball_18.png';
-imageArray[17].style.height  = '100px';
-imageArray[17].style.width = '100px';
-
-imageArray[18] = new Image();
-imageArray[18].src = 'images/magic8ball_19.png';
-imageArray[18].style.height  = '100px';
-imageArray[18].style.width = '100px';
-
-imageArray[19] = new Image();
-imageArray[19].src = 'images/magic8ball_20.png';
-imageArray[19].style.height  = '100px';
-imageArray[19].style.width = '100px';
 
 function addInput(type, placeholder) {
     const input = document.createElement(type);
@@ -122,67 +27,17 @@ function addInput(type, placeholder) {
 
 askButton.addEventListener(('click'), function (event) {
     event.preventDefault();
+    const askInputResponse = form[1].value;
     console.log(randomNumber);
-    switch(randomNumber) {
-        case 1:
-             document.body.appendChild(imageArray[0])
-            break;
-        case 2:
-             document.body.appendChild(imageArray[1])
-            break;
-        case 3:
-             document.body.appendChild(imageArray[2])
-            break;
-        case 4:
-             document.body.appendChild(imageArray[3])
-            break;
-        case 5:
-             document.body.appendChild(imageArray[4])
-            break;
-        case 6:
-             document.body.appendChild(imageArray[5])
-            break;
-        case 7:
-             document.body.appendChild(imageArray[6])
-            break;
-        case 8:
-             document.body.appendChild(imageArray[7])
-            break;
-        case 9:
-             document.body.appendChild(imageArray[8])
-            break;
-        case 10:
-             document.body.appendChild(imageArray[9])
-            break;
-        case 11:
-             document.body.appendChild(imageArray[10])
-            break;
-        case 12:
-             document.body.appendChild(imageArray[11])
-            break;
-        case 13:
-             document.body.appendChild(imageArray[12])
-            break;
-        case 14:
-             document.body.appendChild(imageArray[13])
-            break;
-        case 15:
-             document.body.appendChild(imageArray[14])
-            break;
-        case 16:
-             document.body.appendChild(imageArray[15])
-            break;
-        case 17:
-             document.body.appendChild(imageArray[16])
-            break;
-        case 18:
-             document.body.appendChild(imageArray[17])
-            break;
-        case 19:
-             document.body.appendChild(imageArray[18])
-            break;
-        case 20:
-             document.body.appendChild(imageArray[19])
-            break;
-    }
+    console.log(askInputResponse);
+    form.innerHTML = '';
+    const newAskInput = addInput('input', 'Anything else on your mind?');
+    form.appendChild(askButton2);
 });
+
+let div = document.createElement('div');
+div.id = 'container';
+div.innerText = 'wow';
+div.classname = 'border pad';
+document.body.appendChild(div);
+
