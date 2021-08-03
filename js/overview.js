0cfe1c61ce2bad28cb5f3855e64c149eb0c88c91
Weston Taylor <westontaylor1999@gmail.com>
2021-08-02 17:46:34 -0700

    add: style element to all iamges for height and width; del: console.log askInputResposne, note: looking for solution to save the user input response.

--- js/overview.js
+++ js/overview.js
@@ -9,69 +9,110 @@ askButton.innerText = 'Ask Me';
 
 const askInput = addInput('input', 'Ask me anything!');
 
+
 let randomNumber = Math.floor((Math.random() * 20) + 1);
 
 let imageArray = new Array();
 
 imageArray[0] = new Image();
 imageArray[0].src = 'images/magic8ball_1.png';
+imageArray[0].style.height  = '100px';
+imageArray[0].style.width = '100px';
 
 imageArray[1] = new Image();
 imageArray[1].src = 'images/magic8ball_2.png';
+imageArray[1].style.height  = '100px';
+imageArray[1].style.width = '100px';
 
 imageArray[2] = new Image();
 imageArray[2].src = 'images/magic8ball_3.png';
+imageArray[2].style.height  = '100px';
+imageArray[2].style.width = '100px';
 
 imageArray[3] = new Image();
 imageArray[3].src = 'images/magic8ball_4.png';
+imageArray[3].style.height  = '100px';
+imageArray[3].style.width = '100px';
 
 imageArray[4] = new Image();
 imageArray[4].src = 'images/magic8ball_5.png';
+imageArray[4].style.height  = '100px';
+imageArray[4].style.width = '100px';
 
 imageArray[5] = new Image();
 imageArray[5].src = 'images/magic8ball_6.png';
+imageArray[5].style.height  = '100px';
+imageArray[5].style.width = '100px';
 
 imageArray[6] = new Image();
 imageArray[6].src = 'images/magic8ball_7.png';
+imageArray[6].style.height  = '100px';
+imageArray[6].style.width = '100px';
 
 imageArray[7] = new Image();
 imageArray[7].src = 'images/magic8ball_8.png';
+imageArray[7].style.height  = '100px';
+imageArray[7].style.width = '100px';
 
 imageArray[8] = new Image();
 imageArray[8].src = 'images/magic8ball_9.png';
+imageArray[8].style.height  = '100px';
+imageArray[8].style.width = '100px';
 
 imageArray[9] = new Image();
 imageArray[9].src = 'images/magic8ball_10.png';
+imageArray[9].style.height  = '100px';
+imageArray[9].style.width = '100px';
 
 imageArray[10] = new Image();
 imageArray[10].src = 'images/magic8ball_11.png';
+imageArray[10].style.height  = '100px';
+imageArray[10].style.width = '100px';
 
 imageArray[11] = new Image();
 imageArray[11].src = 'images/magic8ball_12.png';
+imageArray[11].style.height  = '100px';
+imageArray[11].style.width = '100px';
 
 imageArray[12] = new Image();
 imageArray[12].src = 'images/magic8ball_13.png';
+imageArray[12].style.height  = '100px';
+imageArray[12].style.width = '100px';
 
 imageArray[13] = new Image();
 imageArray[13].src = 'images/magic8ball_14.png';
+imageArray[13].style.height  = '100px';
+imageArray[13].style.width = '100px';
 
 imageArray[14] = new Image();
 imageArray[14].src = 'images/magic8ball_15.png';
+imageArray[14].style.height  = '100px';
+imageArray[14].style.width = '100px';
 
 imageArray[15] = new Image();
 imageArray[15].src = 'images/magic8ball_16.png';
+imageArray[15].style.height  = '100px';
+imageArray[15].style.width = '100px';
 
 imageArray[16] = new Image();
 imageArray[16].src = 'images/magic8ball_17.png';
+imageArray[16].style.height  = '100px';
+imageArray[16].style.width = '100px';
 
 imageArray[17] = new Image();
 imageArray[17].src = 'images/magic8ball_18.png';
+imageArray[17].style.height  = '100px';
+imageArray[17].style.width = '100px';
 
 imageArray[18] = new Image();
 imageArray[18].src = 'images/magic8ball_19.png';
+imageArray[18].style.height  = '100px';
+imageArray[18].style.width = '100px';
 
 imageArray[19] = new Image();
 imageArray[19].src = 'images/magic8ball_20.png';
+imageArray[19].style.height  = '100px';
+imageArray[19].style.width = '100px';
 
 function addInput(type, placeholder) {
     const input = document.createElement(type);
@@ -145,5 +186,3 @@ askButton.addEventListener(('click'), function (event) {
             break;
     }
 });
-
- 
